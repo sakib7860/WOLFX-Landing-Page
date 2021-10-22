184587896f9d59e05d1004d5fac0b395c1a4e963
SAKIB7860 <saquibansari556@gmail.com>
2021-10-22 15:28:10 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".main-text h1", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".main-text button", {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
+tl.fromTo(".main-text p", {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
+
+
+    
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
